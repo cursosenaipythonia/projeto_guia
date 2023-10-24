0e026e709c6b57008ca1baff64c0ec887c19d72e
cursosenaipythonia <148901639+cursosenaipythonia@users.noreply.github.com>
2023-10-24 19:48:39 +0000

    upgrade front-end flask, .html .css

--- app.py
+++ app.py
@@ -8,5 +8,20 @@ def index():
   return render_template('index.html')
 
 
+@app.route('/cadastro')
+def cadastro():
+  return render_template('cadastro.html')
+
+
+@app.route('/login')
+def login():
+  return render_template('login.html')
+
+
+@app.route('/guia')
+def guia():
+  return render_template('guia.html')
+
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
